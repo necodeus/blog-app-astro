03473a8501f19c63ee3c0e52f5631b3051cc130f
necodeus <dawid@necodeo.com>
2024-07-24 00:49:40 +0200

    feat: add blog components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
+  output: "server",
+  integrations: [
+    tailwind({
+      nesting: true,
+      applyBaseStyles: true,
+    }),
+  ],
 });
