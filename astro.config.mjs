de02a2c6c001c19c33b4a282655d2f37f7a0f67e
Dawid Smulewicz <dawid@necodeo.com>
2024-08-20 00:53:33 +0200

    astro relase workflow test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    tailwind({
-      nesting: true,
-      applyBaseStyles: true,
-    }),
-  ],
+  integrations: [tailwind({
+    nesting: true,
+    applyBaseStyles: true
+  })],
+  adapter: node({
+    mode: "standalone"
+  })
 });
- 
